3a3175a34171e58898c75de6bfa80e6231da6848
thach.do <ddthach95@gmail.com>
2025-11-29 18:14:15 +0800

    use 1inch api for more reliable api result

--- src/services/liquidityPoolService.ts
+++ src/services/liquidityPoolService.ts
@@ -1,48 +1,33 @@
-const UNISWAP_BASE_URL = 'https://trading-api-labs.interface.gateway.uniswap.org';
-
-// Public API key for Uniswap
-const UNISWAP_API_KEY = 'JoyCGj29tT4pymvhaGciK4r1aIPvqW6W53xT1fwo';
+const ONE_INCH_BASE_URL = 'https://proxy-app.1inch.io/v2.0/fusion/quoter/v2.0/137/quote/receive';
+// Public API key for 1inch
+const ONE_INCH_AUTH_TOKEN = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImFkNzRiNjZhLWJhOTMtNDljYS1iYTk5LWNhYWQ1ZGVjM2E2MSIsImV4cCI6MTc2NDQxNDAwNCwiZGV2aWNlIjoiYnJvd3NlciIsImlhdCI6MTc2NDQxMDQwNH0.fWmslivNaEUt3NNy9IUDiWPqFI9AsBTBs3XRdaqfHu0Rxby5Ih7gLykVKxcGh9GvECP8tze9XiEob72E9STktw';
 
 const BASE_UNIT = 10 ** 6;
 
 export async function swapXsgdToUsdt(amount: number): Promise<number> {
-    const response = await fetch(`${UNISWAP_BASE_URL}/v1/quote`, {
-        method: 'POST',
-        headers: {
-            'accept': '*/*',
-            'accept-language': 'en-US,en;q=0.9,vi;q=0.8',
-            'content-type': 'application/json',
-            'dnt': '1',
-            'origin': 'https://app.uniswap.org',
-            'priority': 'u=1, i',
-            'referer': 'https://app.uniswap.org/',
-            'sec-ch-ua': '"Not_A Brand";v="99", "Chromium";v="142"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"macOS"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-site',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
-            'x-api-key': UNISWAP_API_KEY,
-            'x-request-source': 'uniswap-web',
-            'x-uniquote-enabled': 'true',
-            'x-universal-router-version': '2.0',
-        },
-        body: JSON.stringify(
-            {
-                "type": "EXACT_INPUT",
-                "amount": (amount * BASE_UNIT).toString(),
-                "tokenInChainId": 137,
-                "tokenOutChainId": 137,
-                "tokenIn": "0xdc3326e71d45186f113a2f448984ca0e8d201995",
-                "tokenOut": "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
-                "swapper": "0xAAAA44272dc658575Ba38f43C438447dDED45358",
-                "routingPreference": "FASTEST"
-            }
-        ),
+    const params = new URLSearchParams({
+        walletAddress: '0x0000000000000000000000000000000000000000',
+        fromTokenAddress: '0xdc3326e71d45186f113a2f448984ca0e8d201995',
+        toTokenAddress: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f',
+        amount: (amount * BASE_UNIT).toString(),
     });
+    const url = new URL(ONE_INCH_BASE_URL);
+    url.search = new URLSearchParams(params).toString();
 
+    const response = await fetch(
+        url.toString(),
+        {
+            method: 'GET',
+            headers: {
+                'accept': '*/*',
+                'content-type': 'application/json',
+                'authorization': `Bearer ${ONE_INCH_AUTH_TOKEN}`,
+            },
+        }
+    );
     const data = await response.json();
-    const rawAmount = parseFloat(data.quote.output.amount);
-    return rawAmount / BASE_UNIT;
-};
+    const rawAmount = parseFloat(data.toTokenAmount);
+    const readableAmount = rawAmount / BASE_UNIT;
+
+    return readableAmount;
+}
