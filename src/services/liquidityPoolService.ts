7371c3fee0f1a28281260e997fa62e011fbed71f
thach.do <ddthach95@gmail.com>
2025-11-30 10:52:01 +0800

    put API key in expo .env

--- src/services/liquidityPoolService.ts
+++ src/services/liquidityPoolService.ts
@@ -1,10 +1,15 @@
+import Constants from 'expo-constants';
+
 const ONE_INCH_BASE_URL = 'https://proxy-app.1inch.io/v2.0/fusion/quoter/v2.0/137/quote/receive';
-// Public API key for 1inch
-const ONE_INCH_AUTH_TOKEN = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjZhYmE2YmVlLTk2OGMtNDM2YS1iZWFhLTNhOGM5NjkzZDI4MiIsImV4cCI6MTc2NDQxODA2MywiZGV2aWNlIjoiYnJvd3NlciIsImlhdCI6MTc2NDQxNDQ2M30.HCjnKsjqwWHit0eyLBaDB3J-O5RTKJPudzkX7Y1gmPmRcb_8Sazk7IkAs3CSTQilRnzIrE4To7yz2dDYLZxIsA';
+const ONE_INCH_AUTH_TOKEN = Constants.expoConfig?.extra?.oneInchAuthToken;
 
 const BASE_UNIT = 10 ** 6;
 
 export async function swapXsgdToUsdt(amount: number): Promise<number> {
+    if (!ONE_INCH_AUTH_TOKEN) {
+        throw new Error('No auth token found');
+    }
+
     const params = new URLSearchParams({
         walletAddress: '0x0000000000000000000000000000000000000000',
         fromTokenAddress: '0xdc3326e71d45186f113a2f448984ca0e8d201995',
